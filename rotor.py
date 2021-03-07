1b78a68bc06716e19cf9f097398b944db4381193
Chris Thomas <thegreatkillfile@gmail.com>
2021-03-07 12:03:32 -0500

    Added reverse mapping and an offset placeholder

--- rotor.py
+++ rotor.py
@@ -1,5 +1,23 @@
 class Rotor:
-    def __init__(self, name, mapping):
+    alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    def __init__(self, name, mapping, turnover):
         self.name = name
         self.mapping = mapping
-        
+
+    def setOffset(self, offset):
+        self.offset = offset
+
+    def map(self, input):
+        location = self.alphabet.index(input)
+        mappingResult = self.mapping[location]
+        return self.__logMappingOnReturn(input, mappingResult)
+    
+    def reverseMap(self, input):
+        location = self.mapping.index(input)
+        mappingResult = self.alphabet[location]
+        return self.__logMappingOnReturn(input, mappingResult)
+
+    def __logMappingOnReturn(self, input, output):
+        print("[%s] %s -> %s " %(self.name,input,output))
+        return output
--- rotor_test.py
+++ rotor_test.py
@@ -2,5 +2,30 @@ import rotor
 
 def test_rotor_mappings_are_set_on_init():
     mapping = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    testRotor = rotor.Rotor("test rotor", mapping)
+    testRotor = rotor.Rotor("test rotor", mapping, "A")
     assert testRotor.mapping == mapping
+
+def test_zero_offset_mapping():
+    given = "A"
+    expected = "E"
+    rotorI = __buildRotorI()
+    actual = rotorI.map(given)
+    assert expected == actual
+
+def test_zero_offset_reverse_mapping():
+    given = "E"
+    expected = "A"
+    rotorI = __buildRotorI()
+    actual = rotorI.reverseMap(given)
+    assert expected == actual
+
+def test_offset_setter():
+    rotorI = __buildRotorI()
+    expected = 1
+    rotorI.setOffset(expected)
+    assert rotorI.offset == expected
+
+def __buildRotorI():
+    rotorIMapping = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+    rotorI = rotor.Rotor("Rotor I", rotorIMapping, "A")
+    return rotorI
