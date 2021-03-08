01327d960da7564860e285c32d79af97b4aba8fd
Chris Thomas <thegreatkillfile@gmail.com>
2021-03-07 22:58:32 -0500

    Fixed bidirectional mapping

--- rotor.py
+++ rotor.py
@@ -4,20 +4,28 @@ class Rotor:
     def __init__(self, name, mapping, turnover):
         self.name = name
         self.mapping = mapping
-
-    def setOffset(self, offset):
-        self.offset = offset
+        self.offset = 0
 
     def map(self, input):
-        location = self.alphabet.index(input)
-        mappingResult = self.mapping[location]
-        return self.__logMappingOnReturn(input, mappingResult)
+        return self.__mapCharacterAcrossSetsWithOffset(input, self.alphabet, self.mapping)
     
     def reverseMap(self, input):
-        location = self.mapping.index(input)
-        mappingResult = self.alphabet[location]
-        return self.__logMappingOnReturn(input, mappingResult)
+        return self.__mapCharacterAcrossSetsWithOffset(input, self.mapping, self.alphabet)
+    
+    def __mapCharacterAcrossSetsWithOffset(self, input, inputMap, outputMap):
+        location = self.alphabet.index(input)
+        #apply inbound offset
+        location = location + self.offset
+        #perform mapping
+        mappedCharacter = outputMap[location]
+        mappedLocation = inputMap.index(mappedCharacter)
+        #apply outbound offset
+        offsetMappedLocation = mappedLocation - self.offset
+        return self.alphabet[offsetMappedLocation]
 
     def __logMappingOnReturn(self, input, output):
         print("[%s] %s -> %s " %(self.name,input,output))
         return output
+    
+    def __getCharacterFromMap(self, map, location):
+        return map[(location-self.offset + len(map)) %len(map)]
--- rotor_test.py
+++ rotor_test.py
@@ -19,13 +19,43 @@ def test_zero_offset_reverse_mapping():
     actual = rotorI.reverseMap(given)
     assert expected == actual
 
-def test_offset_setter():
+def test_one_offset_mapping():
+    given = "A"
+    expected = "J"
     rotorI = __buildRotorI()
-    expected = 1
-    rotorI.setOffset(expected)
-    assert rotorI.offset == expected
+    rotorI.offset = 1
+    actual = rotorI.map(given)
+    assert expected == actual
+
+def test_offset_reverse_mapping():
+    given = "K"
+    expected = "D"
+    rotorI = __buildRotorI()
+    rotorI.offset = 1
+    actual = rotorI.reverseMap(given)
+    assert expected == actual
+
+def test_noop_with_zero_offset():
+    given = "A"
+    expected = "A"
+    noopRotor = __buildNoOpRotor()
+    actual = noopRotor.map(given)
+    assert expected == actual
+
+def test_noop_with_one_offset():
+    given = "A"
+    expected = "A"
+    noopRotor = __buildNoOpRotor()
+    noopRotor.offset = 1
+    actual = noopRotor.map(given)
+    assert expected == actual
 
 def __buildRotorI():
     rotorIMapping = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
     rotorI = rotor.Rotor("Rotor I", rotorIMapping, "A")
     return rotorI
+
+def __buildNoOpRotor():
+    rotorMapping = rotor.Rotor.alphabet
+    noopRotor = rotor.Rotor("No Op Rotor", rotorMapping, "A")
+    return noopRotor
