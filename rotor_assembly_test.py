62bcb01e1b55d2298d168d17fa5dba6226d39842
Chris Thomas <thegreatkillfile@gmail.com>
2021-03-11 18:21:36 -0500

    Added integration test for crypting a single char

--- rotor_assembly_test.py
+++ rotor_assembly_test.py
@@ -3,6 +3,21 @@ from rotor_assembly import RotorAssembly
 import rotor
 from alphabet import Alphabet
 
+def test_getRotorPosition():
+    rotors = [
+        rotor.RotorFactory.Rotor("RotorIII"),
+        rotor.RotorFactory.Rotor("RotorII"),
+        rotor.RotorFactory.Rotor("RotorI")
+    ]
+    for i in range(0,3):
+        rotors[i].offset = 0
+    
+    reflector = rotor.RotorFactory.Rotor("ReflectorA")
+    rotorAssembly = RotorAssembly(rotors, reflector)
+
+    positions = rotorAssembly.getRotorPosition()
+    assert positions == [0,0,0]
+    
 position_data = [
     ([0,0,0],0,[0,0,0]), 
     ([0,0,0],1,[1,0,0]),
@@ -10,7 +25,7 @@ position_data = [
     ([21,4,0],1,[22,5,1])
     ]
 @pytest.mark.parametrize("starting, advanceCount, expected", position_data)
-def test_getRotorPosition(starting, advanceCount, expected):
+def test_rotorAdvancement(starting, advanceCount, expected):
     rotors = [
         rotor.RotorFactory.Rotor("RotorIII"),
         rotor.RotorFactory.Rotor("RotorII"),
@@ -26,3 +41,21 @@ def test_getRotorPosition(starting, advanceCount, expected):
 
     positions = rotorAssembly.getRotorPosition()
     assert positions == expected
+
+def test_crypt():
+    rotors = [
+        rotor.RotorFactory.Rotor("RotorIII"),
+        rotor.RotorFactory.Rotor("RotorII"),
+        rotor.RotorFactory.Rotor("RotorI")
+    ]
+    
+    rotors[0].offset = 25
+    rotors[1].offset = 0
+    rotors[2].offset = 0
+    
+    reflector = rotor.RotorFactory.Rotor("ReflectorB")
+    rotorAssembly = RotorAssembly(rotors, reflector)
+
+    actual = rotorAssembly.crypt("A")
+
+    assert "U" == actual
